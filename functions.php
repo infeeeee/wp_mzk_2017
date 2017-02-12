0c8371a70f2c45b2b969c2efb3b058e31e044d3c
infeeeee <gyetpet@gmail.com>
2017-02-12 22:58:30 +0100

    cat name

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ function foo_modify_query_order( $query ) {
         $query->set( 'order', 'ASC' );
     }
 	if ( $query->is_home() && $query->is_main_query()) {
-         $query->set( 'cat', '15');
+         $query->set( 'category_name', 'band');
     }
 }
 add_action( 'pre_get_posts', 'foo_modify_query_order' );
